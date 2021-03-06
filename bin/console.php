a6840a46e8ac4bbb9a4b994fa2584e15f7791b1c
dgafka <dgafka.mail@gmail.com>
2021-03-06 19:40:08 +0100

    Provide set up for Ecotone Lite

---
+++ bin/console.php
@@ -0,0 +1,44 @@
+#!/usr/bin/env php
+<?php
+
+use DI\Container;
+use Ecotone\Lite\EcotoneLiteConfiguration;
+use Ecotone\Lite\GatewayAwareContainer;
+use Ecotone\Messaging\Config\ServiceConfiguration;
+
+$rootCatalog = realpath(__DIR__ . "/..");
+require $rootCatalog . "/vendor/autoload.php";
+
+$container = new class() implements GatewayAwareContainer {
+    private Container $container;
+
+    public function __construct()
+    {
+        $this->container = new Container();
+    }
+
+    public function get($id)
+    {
+        return $this->container->get($id);
+    }
+
+    public function has($id)
+    {
+        return $this->container->has($id);
+    }
+
+    public function addGateway(string $referenceName, object $gateway): void
+    {
+        $this->container->set($referenceName, $gateway);
+    }
+};
+
+$messagingSystem = EcotoneLiteConfiguration::createWithConfiguration(
+    $rootCatalog,
+    $container,
+    ServiceConfiguration::createWithDefaults()
+        ->withLoadCatalog("src"),
+    []
+);
+
+//
